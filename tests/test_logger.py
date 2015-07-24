5bda7f74c49410209c769bbeca3f7837ad808b37
Ian Foote <ian@incuna.com>
2015-07-24 14:16:29 +0100

    Add LogRequestMiddleware

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,9 +1,11 @@
 import datetime
+from unittest.mock import patch
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AnonymousUser, User
 from django.test import RequestFactory, TestCase
 from incuna_test_utils.utils import field_names
 
+from logger.middleware import LogRequestMiddleware
 from logger.models import Log
 
 
@@ -32,3 +34,29 @@ class TestLog(TestCase):
         self.assertIsInstance(log.timestamp, datetime.datetime)
         self.assertEqual(log.url, request.build_absolute_uri())
         self.assertEqual(log.request_method, request.method)
+
+
+class TestLogRequestMiddleware(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.middleware = LogRequestMiddleware()
+
+    def test_authenticated(self):
+        request = self.factory.get('/admin')
+        request.user = User.objects.create()
+
+        with patch('logger.models.LogManager.from_request') as from_request:
+            response = self.middleware.process_request(request)
+
+        from_request.assert_called_once_with(request)
+        self.assertIsNone(response)
+
+    def test_anonymous(self):
+        request = self.factory.get('/admin')
+        request.user = AnonymousUser()
+
+        with patch('logger.models.LogManager.from_request') as from_request:
+            response = self.middleware.process_request(request)
+
+        self.assertFalse(from_request.called)
+        self.assertIsNone(response)
