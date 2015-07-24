7461cc6c6fb8c6a86f4a64085108f0d2eb0400ea
Ian Foote <ian@incuna.com>
2015-07-24 14:01:16 +0100

    Add LogManager.from_request

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,4 +1,7 @@
-from django.test import TestCase
+import datetime
+
+from django.contrib.auth.models import User
+from django.test import RequestFactory, TestCase
 from incuna_test_utils.utils import field_names
 
 from logger.models import Log
@@ -16,3 +19,16 @@ class TestLog(TestCase):
         }
 
         self.assertEqual(field_names(Log), expected_fields)
+
+    def test_log_request(self):
+        factory = RequestFactory()
+        request = factory.get('/admin')
+        request.user = User.objects.create()
+
+        log = Log.objects.from_request(request)
+
+        self.assertEqual(log.user_pk, request.user.pk)
+        self.assertEqual(log.registration_date, request.user.date_joined)
+        self.assertIsInstance(log.timestamp, datetime.datetime)
+        self.assertEqual(log.url, request.build_absolute_uri())
+        self.assertEqual(log.request_method, request.method)
